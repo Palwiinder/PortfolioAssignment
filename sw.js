78bc3344b878a205269ec8a7907d99e326264b1f
palwinder sandhu <palwindersandhu367@gmail.com>
2018-12-20 12:37:23 -0600

    Make little changes

--- sw.js
+++ sw.js
@@ -1,31 +1,32 @@
 const staticCache = 'my-cache-2';
 
 
-self.addEventListener('install', (event) =>{
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(staticCache).then(function(cache){
+        caches.open(staticCache).then(function (cache) {
             return cache.addAll(
-                ["./images/heroimage.jpg",
-                "./images/interior home lighting.jpg",
-                "./images/living room.jpg",
-                "./images/modren kitchen.jpg",
-                "./images/background.jpg",
-                "./images/logo.jpg",
-            "./js/app.js",
-            "./css/styles.css",
-        "/",
-    "/index.html"]
+                ["images/heroimage.jpg",
+                    "images/interior home lighting.jpg",
+                    "images/living room.jpg",
+                    "images/modren kitchen.jpg",
+                    "images/background.jpg",
+                    "images/logo.jpg",
+                    "js/app.js",
+                    "css/styles.css",
+                    "./",
+                    "index.html"
+                ]
             )
         })
     );
 })
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                cacheNames.filter(function(cacheNames) {
+                cacheNames.filter(function (cacheNames) {
                     return cacheNames.startsWith('my-') && cacheNames !== staticCache
-                }).map(function(cacheName) {
+                }).map(function (cacheName) {
                     return caches.delete(cacheName);
                 })
             )
@@ -35,7 +36,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function(response){
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request)
         })
     )
